c92edddacf7cedc4bd9758b3c393ec9bf27c6bca
nazim1971 <md.nazimuddinaj@gmail.com>
2024-10-29 00:37:23 +0600

    explore normal function and arrow function

--- typescriptStart/module1/src/1.6.ts
+++ typescriptStart/module1/src/1.6.ts
@@ -6,4 +6,14 @@ function add (num1:number , num2:number) {
 add(2,10);
 
 //Arrow Function   
-const addArrow = (num1:number, num2:number)
+const addArrow = (num1:number, num2:number): number => num1+num2
+
+//object --> function --> method
+
+const poorUser = {
+    name:"Nazim",
+    balance: 0,
+    addBalance(balance:number): string{
+        return `My New Balance is : ${this.balance+ balance}`;
+    }
+}
